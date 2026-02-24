21dac16371a7cdd9d0f8e262e2f455538893cc3f
DC <tony86777525@gmail.com>
2026-02-24 09:00:33 +0800

    Configures default light theme and disables dark mode

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Filament\Enums\ThemeMode;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -62,6 +63,8 @@ class AdminPanelProvider extends PanelProvider
                 'Projects',
                 'News',
             ])
-            ->viteTheme('resources/css/app.css');
+            ->viteTheme('resources/css/app.css')
+            ->defaultThemeMode(ThemeMode::Light)
+            ->darkMode(false);
     }
 }
