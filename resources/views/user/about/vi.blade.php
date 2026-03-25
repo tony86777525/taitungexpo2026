7f23e0fc18fd30974220ec1f259cbd1cf3051db1
kohakubaibai <hakupai1990@gmail.com>
2026-03-25 18:33:07 +0800

    0325 fix img

--- resources/views/user/about/vi.blade.php
+++ resources/views/user/about/vi.blade.php
@@ -3,7 +3,7 @@
 @push('styles')
     @vite('resources/scss/user/about/vi.scss')
 @endpush
-    
+
 @push('scripts')
     @vite('resources/js/user/about/vi.js')
 @endpush
@@ -32,8 +32,8 @@
                                 <p class="f-p">臺灣國際熱氣球嘉年華已成為台東一年一度的重要盛事，熱氣球更象徵這片土地的獨特魅力。從充氣、燃燒器點燃，到氣囊漸漸成形並緩緩升空，這個過程宛如台東的成長軌跡——醞釀、蓄勢，最終自信綻放，翱翔天際，邁向無限可能。</p>
                                 <p class="f-p">台東博覽會標誌設計以此意象為核心，運用具代表性的「台東藍」作為主色調，並結合熱氣球升起的動態美感，巧妙構築出一個視覺化的驚嘆號。不僅展現台東的活力與蓬勃發展，更象徵這片土地所蘊藏的無限驚喜——壯麗的自然景觀、深厚的人文底蘊、獨特的文化風貌，等待人們探索、感受與共鳴。</p>
                             </div>
-                        </div>    
-                    </div>  
+                        </div>
+                    </div>
                     <div class="introImage wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
                         <img src="{{ asset('images/about/vi/img_element-pc-zh.svg') }}" class="display--pc">
                         <img src="{{ asset('images/about/vi/img_element-mb-zh.svg') }}" class="display--mb">
@@ -46,9 +46,6 @@
 
     {{-- signs start --}}
     <section class="section--signs">
-        <div class="section__title wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.3s">
-            <span class="title f-title-primary">標誌圖形意象</span>
-        </div>
         <div class="section__content">
             <div class="viGroup viGroup--imagery">
                 <div class="container">
