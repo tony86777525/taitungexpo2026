cdce422941b75741b8f59cf00e6d26319ccb0efe
DC <tony86777525@gmail.com>
2026-04-09 23:18:26 +0800

    fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -63,12 +63,12 @@
                                 </video>
                             </div>
                             <!-- Slide 1: webP圖片 -->
-                            <div class="swiper-slide videoFrame">
+                            {{-- <div class="swiper-slide videoFrame">
                                 <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&h=600&fit=crop" alt="Mountain landscape">
                                 <div class="videoFrame__text">
                                     <div class="paragraph">說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字說明文字</div>
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                         <div class="swiper-nav">
                             <div class="swiper-button-prev"></div>
