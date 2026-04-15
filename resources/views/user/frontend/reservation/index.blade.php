eaea1606bf7327c9f3e9903c8fbed77a00074668
kohakubaibai <hakupai1990@gmail.com>
2026-04-15 08:53:47 +0800

    reservation steps

--- resources/views/user/frontend/reservation/index.blade.php
+++ resources/views/user/frontend/reservation/index.blade.php
@@ -19,43 +19,43 @@
         <div class="section__content">
             <div class="container">
                 <div class="steps">
-                    <div class="stepsItem stepsItem--step1">
-                        <div class="title">
-                            <span class="title__number">1</span>
-                            <div class="title__text">
-                                <h3>{{ __('reservation.steps.step1.title') }}</h3>
-                                <span>{{ __('reservation.steps.step1.desc') }}</span>
+                    <div class="steps__item stepItem stepItem--step1">
+                        <div class="stepItem__title">
+                            <span class="number">1</span>
+                            <div class="intro">
+                                <h3 class="intro__topic f-title-secondary">{{ __('reservation.steps.step1.title') }}</h3>
+                                <span class="intro__desc f-p">{{ __('reservation.steps.step1.desc') }}</span>
                             </div>
                         </div>
-                        <ul class="list">
-                            <li>{!! __('reservation.steps.step1.list.list-item1') !!}</li>
+                        <ul class="stepItem__list">
+                            <li class="f-h5">{!! __('reservation.steps.step1.list.list-item1') !!}</li>
                         </ul>
                     </div>
-                    <div class="stepsItem stepsItem--step2">
-                        <div class="title">
-                            <span class="title__number">2</span>
-                            <div class="title__text">
-                                <h3>{{ __('reservation.steps.step2.title') }}</h3>
-                                <span>{{ __('reservation.steps.step2.desc') }}</span>
+                    <div class="steps__item stepItem stepItem--step2">
+                        <div class="stepItem__title">
+                            <span class="number">2</span>
+                            <div class="intro">
+                                <h3 class="intro__topic f-title-secondary">{{ __('reservation.steps.step2.title') }}</h3>
+                                <span class="intro__desc f-p">{{ __('reservation.steps.step2.desc') }}</span>
                             </div>
                         </div>
-                        <ul class="list">
-                            <li>{!! __('reservation.steps.step2.list.list-item1') !!}</li>
-                            <li>{!! __('reservation.steps.step2.list.list-item2') !!}</li>
-                            <li>{!! __('reservation.steps.step2.list.list-item3') !!}</li>
+                        <ul class="stepItem__list">
+                            <li class="f-h5">{!! __('reservation.steps.step2.list.list-item1') !!}</li>
+                            <li class="f-h5">{!! __('reservation.steps.step2.list.list-item2') !!}</li>
+                            <li class="f-h5">{!! __('reservation.steps.step2.list.list-item3') !!}</li>
                         </ul>
                     </div>
-                    <div class="stepsItem stepsItem--step3">
-                        <div class="title">
-                            <span class="title__number">3</span>
-                            <div class="title__text">
-                                <h3>{{ __('reservation.steps.step3.title') }}</h3>
-                                <span>{{ __('reservation.steps.step3.desc') }}</span>
+                    <div class="steps__item stepItem stepItem--step3">
+                        <div class="stepItem__title">
+                            <span class="number">3</span>
+                            <div class="intro">
+                                <h3 class="intro__topic f-title-secondary">{{ __('reservation.steps.step3.title') }}</h3>
+                                <span class="intro__desc f-p">{{ __('reservation.steps.step3.desc') }}</span>
                             </div>
                         </div>
-                        <ul class="list">
-                            <li>{!! __('reservation.steps.step3.list.list-item1') !!}</li>
-                            <li>{!! __('reservation.steps.step3.list.list-item2') !!}</li>
+                        <ul class="stepItem__list">
+                            <li class="f-h5">{!! __('reservation.steps.step3.list.list-item1') !!}</li>
+                            <li class="f-h5">{!! __('reservation.steps.step3.list.list-item2') !!}</li>
                         </ul>
                     </div>
                 </div>
