d03669d639b4e3dd74fa075a3eb2d2675ad56d57
DC <tony86777525@gmail.com>
2026-04-25 12:29:46 +0800

    fix: Fixes 'transpor' typo to 'transport' in traffic view

--- resources/views/user/traffic.blade.php
+++ resources/views/user/traffic.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
     <main class="main">
-        <section class="section section--transpor">
+        <section class="section section--transport">
             <div class="section__title">
                 <div class="container">
                     <span class="title f-title-primary is-pageTitle">{{ __('traffic.page-title') }}</span>
@@ -20,7 +20,7 @@
                 <div class="container">
                     <div class="goal goal--city">
                         <div class="goal__title f-title-primary is-pageTitle">
-                            <span class="title">{{ __('traffic.transpor.goal1.title') }}</span>
+                            <span class="title">{{ __('traffic.transport.goal1.title') }}</span>
                         </div>
                         <div class="goal__content">
                             <ul class="action">
@@ -28,73 +28,73 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="goal goal--transpor">
+                    <div class="goal goal--transport">
                         <div class="goal__title">
-                            <span class="title f-title-primary is-pageTitle">{{ __('traffic.transpor.goal2.title') }}</span>
-                            <div class="desc"><span class="f-p">{{ __('traffic.transpor.goal2.desc') }}</span></div>
+                            <span class="title f-title-primary is-pageTitle">{{ __('traffic.transport.goal2.title') }}</span>
+                            <div class="desc"><span class="f-p">{{ __('traffic.transport.goal2.desc') }}</span></div>
                         </div>
                         <div class="goal__content">
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup1.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup1.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup1.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup1.paragraph') }}</div>
                                     <ul class="list">
-                                        <li><span class="text">{{ __('traffic.transpor.goal2.transporGroup1.list.list1') }}</span></li>
-                                        <li><span class="text">{{ __('traffic.transpor.goal2.transporGroup1.list.list2') }}</span></li>
+                                        <li><span class="text">{{ __('traffic.transport.goal2.transportGroup1.list.list1') }}</span></li>
+                                        <li><span class="text">{{ __('traffic.transport.goal2.transportGroup1.list.list2') }}</span></li>
                                     </ul>
                                     {{-- <ul class="action">
-                                        <li><a href="#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup1.actions.btnText1') }}</span></a></li>
+                                        <li><a href="#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup1.actions.btnText1') }}</span></a></li>
                                     </ul> --}}
                                 </div>
-                                <div class="transporGroup__image">
+                                <div class="transportGroup__image">
                                     <div class="imgWrap">
                                         <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&h=600&fit=crop" alt="">
                                     </div>
                                 </div>
                             </div>
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup2.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup2.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup2.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup2.paragraph') }}</div>
                                     <ul class="action">
-                                        <li><a href="http://ett333023.com.tw/page3.htm" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup2.actions.btnText1') }}</span></a></li>
-                                        <li><a href="https://sdbus.com.tw/" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup2.actions.btnText2') }}</span></a></li>
-                                        <li><a href="https://www.facebook.com/people/%E6%99%AE%E6%82%A0%E7%91%AA%E5%AE%A2%E9%81%8B%E8%82%A1%E4%BB%BD%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8/100063673315341/#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup2.actions.btnText3') }}</span></a></li>
-                                        <li><a href="https://www.taiwanbus.tw/ebuspage/Default.aspx?lan=C" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup2.actions.btnText4') }}</span></a></li>
+                                        <li><a href="http://ett333023.com.tw/page3.htm" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup2.actions.btnText1') }}</span></a></li>
+                                        <li><a href="https://sdbus.com.tw/" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup2.actions.btnText2') }}</span></a></li>
+                                        <li><a href="https://www.facebook.com/people/%E6%99%AE%E6%82%A0%E7%91%AA%E5%AE%A2%E9%81%8B%E8%82%A1%E4%BB%BD%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8/100063673315341/#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup2.actions.btnText3') }}</span></a></li>
+                                        <li><a href="https://www.taiwanbus.tw/ebuspage/Default.aspx?lan=C" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup2.actions.btnText4') }}</span></a></li>
                                     </ul>
                                 </div>
                             </div>
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup3.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup3.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup3.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup3.paragraph') }}</div>
                                     <ul class="action">
-                                        <li><a href="https://www.youbike.com.tw/region/taitung/stations/list/" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup3.actions.btnText1') }}</span></a></li>
+                                        <li><a href="https://www.youbike.com.tw/region/taitung/stations/list/" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup3.actions.btnText1') }}</span></a></li>
                                     </ul>
                                 </div>
                             </div>
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup4.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup4.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup4.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup4.paragraph') }}</div>
                                     <ul class="action">
-                                        <li><a href="#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup4.actions.btnText1') }}</span></a></li>
+                                        <li><a href="#" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup4.actions.btnText1') }}</span></a></li>
                                     </ul>
                                 </div>
                             </div>
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup5.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup5.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup5.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup5.paragraph') }}</div>
                                     <ul class="action">
-                                        <li><a href="https://taisocbus.taitung.gov.tw/mainpage" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transpor.goal2.transporGroup5.actions.btnText1') }}</span></a></li>
+                                        <li><a href="https://taisocbus.taitung.gov.tw/mainpage" class="btn btn--blank is-dark" target="_blank"><span class="btn__text">{{ __('traffic.transport.goal2.transportGroup5.actions.btnText1') }}</span></a></li>
                                     </ul>
                                 </div>
                             </div>
-                            <div class="transporGroup">
-                                <div class="transporGroup__text">
-                                    <div class="title f-title-primary">{{ __('traffic.transpor.goal2.transporGroup6.title') }}</div>
-                                    <div class="paragraph f-p">{{ __('traffic.transpor.goal2.transporGroup6.paragraph') }}</div>
+                            <div class="transportGroup">
+                                <div class="transportGroup__text">
+                                    <div class="title f-title-primary">{{ __('traffic.transport.goal2.transportGroup6.title') }}</div>
+                                    <div class="paragraph f-p">{{ __('traffic.transport.goal2.transportGroup6.paragraph') }}</div>
                                 </div>
                             </div>
                         </div>
